08393a9287f84d555711bd5353021d9fcf64d825
Dan McClory <danmcclory@gmail.com>
2014-01-18 21:20:31 -0500

    clean up the Router spec

--- spec/units/trails/router_spec.rb
+++ spec/units/trails/router_spec.rb
@@ -2,61 +2,59 @@ require 'spec_helper'
 
 describe Trails::Router do
 
-  let(:foo_controller_class) { class_double("FoosController").as_stubbed_const }
+  let!(:foo_controller_class) { class_double("FoosController",
+                                           new: foo_controller).as_stubbed_const }
   let(:env) { { "PATH_INFO" => "/foos" , "REQUEST_METHOD" => "GET"} }
   let(:endpoint) { double( call: [200, {}, ['Success!'] ] ) }
   let(:foo_controller) {
-    instance_double("FoosController")
+    instance_double("FoosController",
+                    resource_name: "foos",
+                    action_for: endpoint
+                   )
   }
   let(:missing_resource_controller) { double }
 
-  describe "#resources" do
-    context "when given a plural resource name" do
-      before do
-        allow(foo_controller_class).to receive(:new).and_return( foo_controller )
-        allow(foo_controller).to receive(:resource_name).and_return("foos")
-      end
-      it "adds a new resource controller to the router's list of controller" do
-        subject.resources :foos
-        expect(subject.controllers).to include(foo_controller)
+  context "when defining the routes" do
+    describe "#resources" do
+      context "when given a plural resource name" do
+        it "adds a new resource controller to the router's list of controller" do
+          subject.resources :foos
+          expect(subject.controllers).to include(foo_controller)
+        end
       end
     end
   end
 
-  describe "#call" do
-    context "route matches Controller & action for an endpoint" do
-      before do
-        subject.controllers = [foo_controller]
-        allow(foo_controller).to receive(:resource_name).and_return("foos")
-        allow(foo_controller).to receive(:action_for).and_return(endpoint)
-      end
-      it "calls the endpoint" do
-         subject.call(env)
-         expect(endpoint).to have_received(:call)
-      end
-    end
-  end
-
-  describe "#controller_for" do
+  context "when dispatching requests to controllers" do
     before do
       subject.controllers = [foo_controller]
       subject.missing_resource_controller = missing_resource_controller
-      allow(foo_controller).to receive(:resource_name).and_return("foos")
     end
 
-    context "route's first segment matches a controller's class name" do
-      let(:route) { "/foos" }
-      it "returns the FoosController" do
-        expect(subject.controller_for(route)).to eq foo_controller
+    describe "#call" do
+      context "route matches Controller & action for an endpoint" do
+        before do
+        end
+        it "calls the endpoint" do
+           subject.call(env)
+           expect(endpoint).to have_received(:call)
+        end
       end
     end
 
-    context "no controller's class name matches the first URL segment" do
-      let(:route) { "/bar" }
-      before do
+    describe "#controller_for" do
+      context "route's first segment matches a controller's class name" do
+        let(:route) { "/foos" }
+        it "returns the FoosController" do
+          expect(subject.controller_for(route)).to eq foo_controller
+        end
       end
-      it "returns a MissingResourceController" do
-        expect(subject.controller_for(route)).to eq missing_resource_controller
+
+      context "no controller's class name matches the first URL segment" do
+        let(:route) { "/bar" }
+        it "returns a MissingResourceController" do
+          expect(subject.controller_for(route)).to eq missing_resource_controller
+        end
       end
     end
   end
