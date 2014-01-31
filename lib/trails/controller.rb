3300afe3215574c9580b4301593310c3b9b4b7b7
Dan McClory <danmcclory@gmail.com>
2014-01-31 13:34:04 -0500

    refactor Controller - pull the rack_app construction into a separate method, make private

--- lib/trails/controller.rb
+++ lib/trails/controller.rb
@@ -10,20 +10,8 @@ module Trails
     def initialize
       @actions = {}
       self.class.public_instance_methods.each do |action|
-        @actions[action] = lambda { |*args|
-          self.params = to_indifferent_access_hash(args.first)
-          self.send(action, *args)
-          [status, headers, body]
-        }
+        @actions[action] = build_rack_app(action)
       end
-    end
-
-    def to_indifferent_access_hash(env)
-      ia_hash = IndifferentAccessHash.new
-      env.each do |key, value|
-        ia_hash[key] = value
-      end
-      ia_hash
     end
 
     def rack_app action
@@ -36,6 +24,23 @@ module Trails
 
     def self.resource_name
       name.to_s.gsub(/Controller/, "").downcase
+    end
+
+    private
+    def build_rack_app(action)
+      lambda { |*args|
+        @params = to_indifferent_access_hash(args.first)
+        send(action, *args)
+        [status, headers, body]
+      }
+    end
+
+    def to_indifferent_access_hash(env)
+      ia_hash = IndifferentAccessHash.new
+      env.each do |key, value|
+        ia_hash[key] = value
+      end
+      ia_hash
     end
 
   end
